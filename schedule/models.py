4a841cbf2a2e47b49f50558f96fe7faa673f6ec8
Aleksandr Sivashev <shyr1punk@gmail.com>
2013-08-23 21:23:38 +0400

    Add __unicode__ method for models

--- schedule/models.py
+++ schedule/models.py
@@ -5,25 +5,40 @@ class AcademicTitle(models.Model):
     a_title_full = models.CharField(max_length=30)
     a_title_short = models.CharField(max_length=5)
 
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s.' % (self.id, self.a_title_short, self.a_title_full)
+
 
 class AcademicDegree(models.Model):
     a_degree_full = models.CharField(max_length=30)
     a_degree_short = models.CharField(max_length=5)
+
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s.' % (self.id, self.a_degree_short, self.a_degree_full)
 
 
 class Faculty(models.Model):
     fac_full = models.CharField(max_length=30)
     fac_short = models.CharField(max_length=10)
 
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s.' % (self.id, self.fac_short, self.fac_full)
+
 
 class Subject(models.Model):
     subj_full = models.CharField(max_length=30)
     subj_short = models.CharField(max_length=10)
 
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s.' % (self.id, self.subj_short, self.subj_full)
+
 
 class Type(models.Model):
     type_full = models.CharField(max_length=30)
     type_short = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s.' % (self.id, self.type_short, self.type_full)
 
 
 class Speciality(models.Model):
@@ -31,17 +46,28 @@ class Speciality(models.Model):
     spec_short = models.CharField(max_length=10)
     faculty = models.ForeignKey(Faculty)
 
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s.' % (self.id, self.spec_short, self.spec_full)
+
 
 class Chair(models.Model):
     cha_full = models.CharField(max_length=50)
     cha_short = models.CharField(max_length=10)
     faculty = models.ForeignKey(Faculty)
 
+    def __unicode__(self):
+        return u'ID: %d, Short title: %s, full title: %s, faculty: %s' % \
+               (self.id, self.cha_full, self.cha_short, self.faculty.fac_short)
+
 
 class Group(models.Model):
     title = models.CharField(max_length=10)
     course = models.IntegerField()
     spec = models.ForeignKey(Speciality)
+
+    def __unicode__(self):
+        return u'ID: %d, Title: %s, course: %s, speciality: %s' % \
+               (self.id, self.title, self.course, self.spec.spec_short)
 
 
 class Teacher(models.Model):
@@ -50,9 +76,16 @@ class Teacher(models.Model):
     chair = models.ForeignKey(Chair)
     name = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return u'ID: %d, name: %s, degree: %s, title: %s, chair: %s' % \
+            (self.id, self.name, self.academic_degree.a_degree_short, self.academic_title.a_title_short, self.chair.cha_short)
+
 
 class Auditory(models.Model):
     title = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return u'ID %d, number: %s' % (self.id, self.title)
 
 
 class Lesson(models.Model):
@@ -63,3 +96,8 @@ class Lesson(models.Model):
     lesson_type = models.ForeignKey(Type)
     group = models.ForeignKey(Group)
     auditory = models.ForeignKey(Auditory)
+
+    def __unicode__(self):
+        return u'ID %d, number: %s, date: %s, subject: %s, teacher: %s, type: %s, group: %s, auditory: %s ' % \
+               (self.id, self.number, self.date, self.subject.subj_full, self.teacher.name,
+                self.lesson_type.type_full, self.group.title, self.auditory.title)
